5ec5e842b42283c0b30cb4e36c4aa0e9d129eec8
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-04-05 22:42:24 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+with open("README.md", "r") as fin:
+    long_description = fin.read()
+
+setuptools.setup(
+    name='clip-util',
+    version='0.0.1',
+    author='Kyle L. Davis',
+    author_email='AceofSpades5757.github@gmail.com',
+    url='https://github.com/AceofSpades5757/clip-util',
+    description='Utilities for use with Python.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    python_requires='>=3.6',
+    packages=setuptools.find_packages('src'),
+    package_dir={
+        '': 'src',
+        'clipboard': 'src/clipboard',
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
