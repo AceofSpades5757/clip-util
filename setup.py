fb80b6aff39ab320bc3e2472b80858c688f5b4c5
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-05-24 22:21:01 -0500

    Update classifiers

--- setup.py
+++ setup.py
@@ -4,12 +4,17 @@ import setuptools
 with open("README.md", "r") as fin:
     long_description = fin.read()
 
+
 setuptools.setup(
     name='clip-util',
     version='0.1.1',
     author='Kyle L. Davis',
     author_email='AceofSpades5757.github@gmail.com',
     url='https://github.com/AceofSpades5757/clip-util',
+    project_urls={
+        "Documentation": "https://clip-util.readthedocs.io/en/latest/",
+        "Author": "https://github.com/AceofSpades5757",
+    },
     description='Utilities for use with Python.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -24,9 +29,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: Microsoft :: Windows",
         'License :: OSI Approved :: MIT License',
-        # 'Programming Language :: Python :: 3.6',
-        # 'Programming Language :: Python :: 3.7',
-        # 'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
