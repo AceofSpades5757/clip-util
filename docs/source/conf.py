5108265339f7b3cce502fb1078ef2fc61f12d4be
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-05-25 19:57:19 -0500

    Run formatter

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,9 +13,9 @@
 # sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 
-project = 'clip-util'
-copyright = '2022, Kyle L. Davis'
-author = 'Kyle L. Davis'
+project = "clip-util"
+copyright = "2022, Kyle L. Davis"
+author = "Kyle L. Davis"
 
 
 # -- General configuration ---------------------------------------------------
@@ -24,11 +24,11 @@ author = 'Kyle L. Davis'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -46,4 +46,4 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
