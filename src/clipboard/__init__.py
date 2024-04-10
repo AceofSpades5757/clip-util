d76ce53890079188f4bd0d94143d3880f8e736cc
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2024-04-10 13:36:42 -0500

    refactor: add errors to root public api

--- src/clipboard/__init__.py
+++ src/clipboard/__init__.py
@@ -4,13 +4,27 @@ from clipboard.clipboard import get_clipboard
 from clipboard.clipboard import set_clipboard
 from clipboard.clipboard import get_available_formats
 from clipboard.html_clipboard import HTML_ENCODING
+from clipboard.errors import ClipboardError
+from clipboard.errors import EmptyClipboardError
+from clipboard.errors import FormatNotSupportedError
+from clipboard.errors import GetClipboardError
+from clipboard.errors import OpenClipboardError
+from clipboard.errors import SetClipboardError
 
 
 __all__ = [
     "Clipboard",
     "ClipboardFormat",
     "HTML_ENCODING",
+    # Convenience Functions
+    "get_available_formats",
     "get_clipboard",
     "set_clipboard",
-    "get_available_formats",
+    # Errors
+    "ClipboardError",
+    "EmptyClipboardError",
+    "FormatNotSupportedError",
+    "GetClipboardError",
+    "OpenClipboardError",
+    "SetClipboardError",
 ]
