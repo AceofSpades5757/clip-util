19ccee2650c3d0f5cf144432ff329ec058cabb0b
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2024-04-10 10:33:20 -0500

    feat: add new function to root public api

--- src/clipboard/__init__.py
+++ src/clipboard/__init__.py
@@ -2,6 +2,7 @@ from clipboard.clipboard import Clipboard
 from clipboard.clipboard import ClipboardFormat
 from clipboard.clipboard import get_clipboard
 from clipboard.clipboard import set_clipboard
+from clipboard.clipboard import get_available_formats
 from clipboard.html_clipboard import HTML_ENCODING
 from clipboard.html_clipboard import HTMLTemplate
 
@@ -13,4 +14,5 @@ __all__ = [
     "HTMLTemplate",
     "get_clipboard",
     "set_clipboard",
+    "get_available_formats",
 ]
