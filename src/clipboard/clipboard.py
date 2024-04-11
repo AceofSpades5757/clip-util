e34419b732d999929b8799dfef99e27d1fbdc859
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2024-04-11 10:57:27 -0500

    fix: fix type hints

--- src/clipboard/clipboard.py
+++ src/clipboard/clipboard.py
@@ -64,7 +64,7 @@ if os.environ.get("LOGLEVEL"):
     logger.addHandler(file_handler)
 
 
-def get_clipboard(format: Union[int, ClipboardFormat] = None) -> Optional[str]:
+def get_clipboard(format: Optional[Union[int, ClipboardFormat]] = None) -> Optional[Union[str, bytes]]:
     """Conveniency wrapper to get clipboard.
 
     Instead of using the `Clipboard.default_format`, this function uses the
